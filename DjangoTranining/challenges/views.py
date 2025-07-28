f18d51d2b75b59894be4fa4aecd7b86fa75c660a
Franco-Mina <franco.mina15@gmail.com>
2025-07-28 09:28:37 -0300

    added first challenges views

--- DjangoTranining/challenges/views.py
+++ DjangoTranining/challenges/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def january(request):
+    return HttpResponse("This is the challenge for january!")
+
+def february(request):
+    return HttpResponse("This is the challenge for february!")
