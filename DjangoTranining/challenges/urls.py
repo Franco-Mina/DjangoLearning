260b9c7c0a961b9a217f1d57dbd6cf2ee989e983
Franco-Mina <franco.mina15@gmail.com>
2025-08-02 15:12:02 -0300

    added list with months and links

--- DjangoTranining/challenges/urls.py
+++ DjangoTranining/challenges/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("",views.monthly_challenge_list),
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
